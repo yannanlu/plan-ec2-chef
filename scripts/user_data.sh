642671b7d9d3fc50d8015e2f586a2e6b6371bf9c
yannanlu <yannanlu@yahoo.com>
2018-01-12 22:38:24 -0500

    added retry to user_data.sh on installation of chef

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -14,4 +14,19 @@ else
   fi
 fi
 
-curl -LO https://www.opscode.com/chef/install.sh && sudo bash ./install.sh -v 13.2.20
+curl -LO https://www.opscode.com/chef/install.sh
+for retry in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
+  if [ -s ./install.sh ]; then
+    echo "`date` run install.sh at $retry" >> /tmp/install_chef.log
+    sudo bash ./install.sh -v 13.2.20
+    if [ -f /opt/chef/embedded/bin/chef-solo ]; then
+      echo "`date` ./install.sh completed at $retry" >> /tmp/install_chef.log
+      break
+    fi
+  elif [ -e ./install.sh ]; then
+    rm ./install.sh
+    echo "`date` empty install.sh at $retry" >> /tmp/install_chef.log
+    sleep 10
+    curl -LO https://www.opscode.com/chef/install.sh
+  fi
+done
